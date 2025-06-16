cc9a859eb25dffda6f5550abf13f18302cff4126
Wellington Correia Borsato Jr. <wellington.borsato@yahoo.com.br>
2025-06-16 15:43:48 -0300

    Testando o Swiper Com HTML e CSS

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const swiper = new Swiper('.swiper', {
+      spaceBetween: 20,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      autoplay: {
+        delay: 3000,
+      },
+      loop: true,
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+        },
+        500: {
+          slidesPerView: 2,
+        },
+        768: {
+          slidesPerView: 3,
+        },
+        1024: {
+          slidesPerView: 4,
+        },
+      },
+    });
